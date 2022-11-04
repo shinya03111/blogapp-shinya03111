5a5f3f807117d7af3a6f3e6319487226c4486f1b
谷本新也 <shinya03111@gmail.com>
2022-11-05 07:09:52 +0900

    ユーザが存在することを前提にしてコードを修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -27,7 +27,11 @@ class Article < ApplicationRecord
     belongs_to :user
 
     def display_created_at
-        I18n.l(self.created_at, format: :default)
+      I18n.l(self.created_at, format: :default)
+    end
+
+    def author_name
+      user.display_name
     end
 
     private
