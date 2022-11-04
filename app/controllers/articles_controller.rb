65db57f9dd390dfe630020132dd3389bc53820e9
谷本新也 <shinya03111@gmail.com>
2022-11-05 05:22:04 +0900

    current_userとして再定義

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update]
+  before_action :set_article, only: [:show]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
  def index
@@ -24,19 +24,21 @@ class ArticlesController < ApplicationController
  end
 
  def edit
+  @article = current_user.articles.find(params[:id])
  end
 
  def update
-   if @article.update(article_params)
-      redirect_to article_path(@article), notice: '更新できました'
-   else
-      flash.now[:error] = '更新できませんでした'
-      render :edit
-   end
+  @article = current_user.articles.find(params[:id])
+  if @article.update(article_params)
+    redirect_to article_path(@article), notice: '更新できました'
+  else
+    flash.now[:error] = '更新できませんでした'
+    render :edit
+  end
  end
 
  def destroy
-   article = Article.find(params[:id])
+   article = current_user.articles.find(params[:id])
    article.destroy!
    redirect_to root_path, notice: '削除に成功しました'
  end
