f2b78d5621ca0265aeee9250d4e82af166afeb67
谷本新也 <shinya03111@gmail.com>
2022-11-13 06:44:28 +0900

    アイキャッチの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,35 +14,37 @@
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
-    validates :title, presence: true
-    validates :title, length: { minimum: 2, maximum: 100 }
-    validates :title, format: { with: /\A(?!\@)/  }
+  has_one_attached :eyecatch
 
-    validates :content, presence: true
-    validates :content, length: { minimum: 10 }
-    validates :content, uniqueness: true
+  validates :title, presence: true
+  validates :title, length: { minimum: 2, maximum: 100 }
+  validates :title, format: { with: /\A(?!\@)/  }
 
-    validate :validate_title_and_content_length
+  validates :content, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :content, uniqueness: true
 
-    has_many :comments, dependent: :destroy
-    has_many :likes, dependent: :destroy
-    belongs_to :user
+  validate :validate_title_and_content_length
 
-    def display_created_at
-      I18n.l(self.created_at, format: :default)
-    end
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  belongs_to :user
 
-    def author_name
-      user.display_name
-    end
+  def display_created_at
+    I18n.l(self.created_at, format: :default)
+  end
 
-    def like_count
-      likes.count
-    end
+  def author_name
+    user.display_name
+  end
 
-    private
-    def validate_title_and_content_length
-        char_count = self.title.length + self.content.length
-        errors.add(:content, '100文字以上で！') unless char_count > 100
-    end
+  def like_count
+    likes.count
+  end
+
+  private
+  def validate_title_and_content_length
+      char_count = self.title.length + self.content.length
+      errors.add(:content, '100文字以上で！') unless char_count > 100
+  end
 end
